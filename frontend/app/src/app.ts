07fdd14506ba8d557132ff81270f596261d89582
vangola-silver <38697849+vangola-silver@users.noreply.github.com>
2023-10-16 19:08:44 +0200

    added replica visualization for frontend uuids generated per frontend instance and show in console for each req

--- frontend/app/src/app.ts
+++ frontend/app/src/app.ts
@@ -3,15 +3,22 @@ import express from "express";
 import compression from "compression";
 
 import path from 'path';
-import fs from 'fs';
+import { v4 } from "uuid";
 
 const app = express();
+const uuid = v4();
 
 app.use(express.json());
 app.use(compression());
 const pathToStatic = path.join(__dirname, '/../web');
+// Custom middleware for logging served files
+app.use((req, res, next) => {
+    console.log(`File ${req.url} served by frontend: ${uuid}`)
+    next();
+});
 
 app.use(express.static(pathToStatic))
+
 
 const port = process.env.SERVER_PORT || 4000;
 
